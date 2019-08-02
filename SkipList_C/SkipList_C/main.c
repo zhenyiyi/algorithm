9581f85dfca52f18fdd614ebc37deb4073d9a49e
wenhua.zhang <wenhua.zhang@quanshi.com>
2019-08-02 09:19:25 +0800

    SkipList_C

---
+++ SkipList_C/SkipList_C/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SkipList_C
+//
+//  Created by fenglin on 2019/8/1.
+//  Copyright © 2019 quanshi. All rights reserved.
+//
+
+#include <stdio.h>
+#include "skiplist_c/skiplist.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    testSkipList();
+    
+    return 0;
+}
